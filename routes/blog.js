ca60cf905abca5b6ad40515e764b114ca31e60ac
HimeshPremji <himeshdmaheswary@gmail.com>
2025-02-28 22:03:25 +0500

    Blog almost completed, adjusting comments section below the blog

--- routes/blog.js
+++ routes/blog.js
@@ -4,6 +4,7 @@ const path = require('path');
 const router = Router();
 
 const Blog = require('../models/blog');
+const Comment = require('../models/comment');
 const { resolve4 } = require('dns');
 
 const storage = multer.diskStorage({
@@ -28,6 +29,16 @@ router.get('/:id', async (req, res) => {
     user: req.user,
     blog,
   });
+});
+
+router.post('/comment/:blogId', async (req, res) => {
+  await Comment.create({
+    content: res.body.content,
+    blogId: req.params.blogId,
+    createdBy: req.user._id,
+    profileImage: req.user.profileImageURL,
+  });
+  return res.redirect(`/blog/${req.params.blogId}`);
 });
 
 router.post('/', upload.single('coverImage'), async (req, res) => {
